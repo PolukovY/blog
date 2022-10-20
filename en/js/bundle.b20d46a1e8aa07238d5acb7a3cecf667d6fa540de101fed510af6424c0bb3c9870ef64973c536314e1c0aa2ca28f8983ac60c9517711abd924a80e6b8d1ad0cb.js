7f807f8ca6783d78d74c33c8cd05911da8b62e34
PolukovY <PolukovY@users.noreply.github.com>
2022-10-20 19:15:55 +0000

    deploy: 5ccb69a4d2c6adb6029a004016a2d32ebe9be584

--- en/js/bundle.b20d46a1e8aa07238d5acb7a3cecf667d6fa540de101fed510af6424c0bb3c9870ef64973c536314e1c0aa2ca28f8983ac60c9517711abd924a80e6b8d1ad0cb.js
+++ en/js/bundle.b20d46a1e8aa07238d5acb7a3cecf667d6fa540de101fed510af6424c0bb3c9870ef64973c536314e1c0aa2ca28f8983ac60c9517711abd924a80e6b8d1ad0cb.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'https://blog.techworld-with-yevgen.com/';
+const baseURL = 'https://techworld-with-yevgen.com/blog/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
